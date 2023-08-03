c6033a9a6033679ed3ea0e327d776bfc23d69108
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-03 16:10:12 -0300

    feat: implementação inicial de rotas

--- desafioLacrei/src/main.jsx
+++ desafioLacrei/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import './global.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+import Usuario from "./Pages/Usuario/Usuario.jsx";
+import Profissional from "./Pages/Profissional/Profissional.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/usuario",
+        element: <Usuario />,
+      },
+      {
+        path: "/profissional",
+        element: <Profissional />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
