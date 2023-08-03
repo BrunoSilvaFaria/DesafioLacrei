e6a4c94a67bc7cfc0cbff444257f1eff00caa8fc
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-03 16:07:51 -0300

    feat: adição do menu a página

---
+++ desafioLacrei/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Nav/Menu/Nav";
+function Header() {
+  return (
+    <>
+      <h1>Titulo</h1>
+      <img src="" alt="Logo do site" />
+      <Nav />
+    </>
+  );
+}
+
+export default Header;
