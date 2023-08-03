a7fae60fed36071315e5302672fefda5d3364d55
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-03 16:07:35 -0300

    feat: adição do footer da página

---
+++ desafioLacrei/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Nav from "../Nav/Menu/Nav";
+import Social from "../Nav/Social/Social";
+function Footer() {
+  return (
+    <>
+      <Nav />
+      <Social />
+      <p>Desafio Front-end Lacrei</p>
+    </>
+  );
+}
+
+export default Footer;
