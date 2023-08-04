50f44294e62d8778b07cd1b76a2e063f82edfd0a
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-04 16:10:16 -0300

    feat: atualização nas rotas

--- desafioLacrei/src/main.jsx
+++ desafioLacrei/src/main.jsx
@@ -1,16 +1,21 @@
 import ReactDOM from "react-dom/client";
-import './global.css';
+import "./global.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import Usuario from "./Pages/Usuario/Usuario.jsx";
 import Profissional from "./Pages/Profissional/Profissional.jsx";
+import Home from "./Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "/usuario",
         element: <Usuario />,
